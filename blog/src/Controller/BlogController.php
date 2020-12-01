4a12fe2d954e0e84dcd8593c0df6333974f025e8
Mickael-DP <m.dallepasqualine@gmail.com>
2020-12-01 16:54:04 +0100

    tutos - les-entites

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -15,4 +15,28 @@ class BlogController extends AbstractController
     {
        return $this->render('blog/index.html.twig');
     }
+
+    public function add()
+    {
+       return $this->render('blog/add.html.twig');
+    }
+
+    public function show($url)
+    {
+       return $this->render('blog/show.html.twig',[
+           'slug' => $url
+       ]);
+    }
+
+    public function edit($id)
+    {
+        return $this->render('blog/edit.html.twig',[
+            'slug' => $id
+        ]);
+    }
+
+    public function remove($id)
+    {
+       return new Response("<h1>Supprimer l'article " .$id. "</h1>");
+    }
 }
